a70b12643664866dec2ec4522bd018cb88688891
Jesse Hallett <jesse@sitr.us>
2022-10-02 13:56:30 -0400

    set background color to blue

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use redstone::RedstonePlugin;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.43, 0.69, 1.0))) // sky color
         .insert_resource(ImageSettings::default_nearest())
         .add_plugins(DefaultPlugins)
         .add_plugin(RedstonePlugin)
